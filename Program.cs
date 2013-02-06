caed7a9ebb3a231ded42d3d5230bd7b42e0ee397
Sven Poppelmann <github@kalayl.net>
2013-02-06 19:39:39 +0000

    project creation and sample app

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Sirit.Data;
+using Sirit.Driver;
+using Sirit.Mapping;
+
+namespace rfid
+{
+    class Program
+    {
+        /// <summary>
+        /// IP Address of the reader
+        /// </summary>
+        private string m_ipAddress = "192.168.0.118";
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Program()
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="ipAddress">IP Address of the reader</param>
+        public Program(string ipAddress)
+        {
+            m_ipAddress = ipAddress;
+        }
+
+        /// <summary>
+        /// Executes the program
+        /// </summary>
+        public void Run()
+        {
+            try
+            {
+                // Open a connection to the reader
+                DataManager dataManager = new DataManager(DataManager.ConnectionTypes.SOCKET, m_ipAddress, 0);
+                dataManager.OpenConnection();
+                Console.WriteLine("Connection Opened");
+
+                // Get the reader's name
+                InfoManager infoManager = new InfoManager(dataManager);
+                String v = infoManager.Name;
+                Console.WriteLine("Name: " + v);
+                infoManager = null;
+
+                // Login as administrator
+                ReaderManager readerManager = new ReaderManager(dataManager);
+                if (!readerManager.Login("admin", "readeradmin"))
+                    throw new Exception("Login attempt failed: " + readerManager.LastErrorMessage);
+                v = readerManager.WhoAmI();
+                Console.WriteLine("Login: " + v);
+
+                // Open an event channel and get it's ID
+                String id = dataManager.GetEventChannel(new EventFound(EventReceivedHandler));
+                Console.WriteLine("Event Channel ID: " + id);
+
+                // Register for event.tag.report
+                if (!readerManager.EventsRegister(id, "event.tag.report"))
+                    throw new Exception("Failure to register for event: " + readerManager.LastErrorMessage);
+                Console.WriteLine("Registered for event.tag.report");
+
+                // Set operating mode to active
+                SetupManager setupManager = new SetupManager(dataManager);
+                setupManager.OperatingMode = SetupManager.OperatingModeTypes.ACTIVE;
+                Console.WriteLine("Operating Mode: Active");
+
+                // Sleep while handling tag events
+                Thread.Sleep(500);
+
+                // Set operating mode to standby
+                setupManager.OperatingMode = SetupManager.OperatingModeTypes.STANDBY;
+                Console.WriteLine("Operating Mode: Standby");
+
+                // Unregister for event.tag.report
+                if (!readerManager.EventsUnregister(id, "event.tag.report"))
+                    throw new Exception("Failure to unregister for event: " + readerManager.LastErrorMessage);
+                Console.WriteLine("Unregistered for event.tag.report");
+
+                // Close the connection
+                setupManager = null;
+                readerManager = null;
+                dataManager.Close();
+                Console.WriteLine("Connection Closed");
+                Console.ReadLine();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Error: " + exc.Message);
+            }
+        }
+
+        /// <summary>
+        /// Main method for the application
+        /// </summary>
+        /// <param name="args">Command line arguments
+        /// first argument - IP Address of the reader (optional)</param>
+        static void Main(string[] args)
+        {
+            // Create the program object
+            Program prog;
+            if (args.Length > 0)
+                prog = new Program(args[0]);
+            else
+                prog = new Program();
+            // Execute the application
+            prog.Run();
+            Console.WriteLine("Exiting");
+        }
+
+        /// <summary>
+        /// EventFound delegate is used to notify interested entities whenever an 
+        /// event is triggered on the reader
+        /// </summary>
+        public void EventReceivedHandler(object sender, EventInfo eventInfo)
+        {
+            if (eventInfo.Type == EventInfo.EventTypes.TAG_REPORT)
+            {
+                string tagid = eventInfo.GetParameter(EventInfo.EventTagReportParams.TagId);
+                if (tagid != null)
+                    Console.WriteLine("TagID: " + tagid);
+            }
+        }
+    }
+}
